734c7d3dece49017d4db87b824c0a62f934cfb6a
github username <github email address>
2019-02-18 11:55:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution here:
+// Code your solution her
+
+function driversWithRevenueOver(array, revenue) {
+
+      return array.filter(function(driver) {
+
+          return driver['revenue'] > revenue;
+
+      });
+
+}
+
+function driverNamesWithRevenueOver(array, revenue) {
+
+
+return driversWithRevenueOver(array, revenue).map(function(driver) {
+
+      return  driver.name;
+
+
+  });
+
+
+
+}
+
+function exactMatch (array, match) {
+  return array.filter(function (driver) {
+    let matches = false;
+
+    for (const key in match) {
+      matches = driver[key] === match[key];
+    }
+
+    return matches;
+  });
+}
+
+
+function exactMatchToList(array, match) {
+
+  return exactMatch(array, match).map(function(driver) {
+
+      return driver.name;
+  });
+
+
+
+}
